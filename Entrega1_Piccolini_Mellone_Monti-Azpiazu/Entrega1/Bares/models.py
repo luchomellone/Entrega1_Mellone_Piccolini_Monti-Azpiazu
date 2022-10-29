82262b531bc1416c077be77d92179c8814860f1d
Luciano Mellone <luchomellone@gmail.com>
2022-10-29 17:56:37 -0300

    Avance proyecto

--- Entrega1_Piccolini_Mellone_Monti-Azpiazu/Entrega1/Bares/models.py
+++ Entrega1_Piccolini_Mellone_Monti-Azpiazu/Entrega1/Bares/models.py
@@ -1,3 +1,21 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+class Bares (models.Model):
+
+    nombre = models.CharField(max_length = 100)
+    email = models.EmailField()
+    telefono = models.IntegerField()
+
+class Heladerias (models.Model):
+
+    nombre = models.CharField(max_length = 100)
+    email = models.EmailField()
+    telefono = models.IntegerField()
+
+class Restaurantes (models.Model):
+
+    nombre = models.CharField(max_length = 100)
+    email = models.EmailField()
+    telefono = models.IntegerField()
+
--- Entrega1_Piccolini_Mellone_Monti-Azpiazu/Entrega1/Bares/views.py
+++ Entrega1_Piccolini_Mellone_Monti-Azpiazu/Entrega1/Bares/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Bares, Heladerias, Restaurantes
 
-# Create your views here.
+def bar (request, nombre, email, telefono):
+
+    return 
+
+def restaurant (request, nombre, email, telefono):
+
+    return 
+
+def heladeria (request, nombre, email, telefono):
+
+    return 
+
